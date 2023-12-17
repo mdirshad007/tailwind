e5d9f49868f89bf475c5eac59bf5a485c8d72a2c
mdirshad007 <md.irshad8447@gmail.com>
2023-12-17 21:35:16 +0530

    project send

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'Heading': ['Montserrat', 'sans-serif'],
+      'BodyFont': ['Lato', 'sans-serif'],
+    },
+  },    
+  plugins: [],
+}
+
